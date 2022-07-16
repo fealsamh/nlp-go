87c02023213dfd3be4f4a67685907fae88d9ea5e
PeterH <phomola@uoc.edu>
2022-07-16 15:10:43 +0100

    Bot entities (#12)
    * added entities to bot
    
    * fixed bug
    
    * modules tidied
    
    * added entities to response
    
    Co-authored-by: Petr Homola <petr.homola@amio.io>
    Co-authored-by: Petr Homola <homola.p@gmail.com>

--- aibot/recogniseintent.go
+++ aibot/recogniseintent.go
@@ -11,16 +11,17 @@ type IntentSimilarity struct {
 }
 
 type RecogniseIntentResponse struct {
-	Intents []IntentSimilarity `json:"intents"`
+	Intents  []IntentSimilarity  `json:"intents"`
+	Entities map[string][]string `json:"entities"`
 }
 
-func (c *Client) RecogniseIntent(botId, text string) ([]IntentSimilarity, error) {
+func (c *Client) RecogniseIntent(botId, text string) ([]IntentSimilarity, map[string][]string, error) {
 	var r *RecogniseIntentResponse
 	if _, err := c.callServicePost("/api/v1/bots/intents", &RecogniseIntentRequest{
 		BotID: botId,
 		Text:  text,
 	}, &r); err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return r.Intents, nil
+	return r.Intents, r.Entities, nil
 }
