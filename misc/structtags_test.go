daa166a5632dc1a79c38a009e474faef4fb09411
Petr Homola <petr.homola@amio.io>
2022-07-31 21:57:44 +0100

    added XMLName tag check

--- misc/structtags_test.go
+++ misc/structtags_test.go
@@ -1,6 +1,7 @@
 package misc
 
 import (
+	"encoding/xml"
 	"reflect"
 	"testing"
 
@@ -16,6 +17,11 @@ type StructJson2 struct {
 	SomeField int `json:"somefield"`
 }
 
+type StructXML1 struct {
+	XMLName xml.Name `xml:"struct_xml1"`
+	Name    string   `xml:"name"`
+}
+
 func TestCheckJSONTags(t *testing.T) {
 	assert := assert.New(t)
 
@@ -25,4 +31,11 @@ func TestCheckJSONTags(t *testing.T) {
 	err = CheckJSONTags(reflect.TypeOf((*StructJson2)(nil)), "id")
 	assert.NotNil(err)
 	assert.Equal(err.Error(), "bad 'json' struct tag for misc.StructJson2.SomeField (some_field != somefield)")
+}
+
+func TestCheckXMLTags(t *testing.T) {
+	assert := assert.New(t)
+
+	err := CheckXMLTags(reflect.TypeOf((*StructXML1)(nil)), "id")
+	assert.Nil(err)
 }
